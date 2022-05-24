5ba0f14f1dc60b7b34c9345448325dafe7391f40
Aerex <noamfo@aerex.me>
2022-05-24 13:45:33 -0500

    chore: Added use strict mode to plugin.ts

--- src/ibmcloud/plugin.ts
+++ src/ibmcloud/plugin.ts
@@ -13,6 +13,9 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 **/
+
+'use strict'; 
+
 import { SystemCommand } from '../util/SystemCommand';
 import * as semver from 'semver';
 import { IBMCloud } from '../consts';
